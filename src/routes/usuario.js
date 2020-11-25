93da198adac89d1661c02604a9e40eb8ea346006
David Mendoza Loor <davidmendozaloor@MacBook-Pro-de-David.local>
2020-11-25 15:22:10 -0500

    Cambio en oficina

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -1,10 +1,10 @@
 import express from "express";
 import {
-  buscarTodos,
-  buscarPorId,
-  crearUsuario,
-  actualizarUsuario,
-  eliminarUsuario
+    buscarTodos,
+    buscarPorId,
+    crearUsuario,
+    actualizarUsuario,
+    eliminarUsuario
 } from "../controllers/usuario";
 import { checkCrearUsuario } from "../validations/usuario";
 import { allowedMethods, asyncWrapper, checkParameters } from "../utils/error";
@@ -15,9 +15,9 @@ const router = express.Router();
 router.use("/$", allowedMethods(["GET", "POST"]));
 router.get("/", asyncWrapper(buscarTodos));
 router.post(
-  "/",
-  checkParameters(checkCrearUsuario),
-  asyncWrapper(crearUsuario)
+    "/",
+    //checkParameters(checkCrearUsuario),
+    asyncWrapper(crearUsuario)
 );
 
 router.use("/:id$", allowedMethods(["GET", "PUT", "DELETE"]));
