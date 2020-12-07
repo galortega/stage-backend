759bd2a2bc1d8bae4737607a07e08c8fe7b4b82e
galortega <galfeort@espol.edu.ec>
2020-12-07 15:37:28 -0500

    payload to large

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../public")));
 
 // Routes
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "10mb", extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
